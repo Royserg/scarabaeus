93938d4816e15384f3f11b1cccfad03c5f1b1d2a
Royserg <staniojakub@gmail.com>
2024-03-23 15:57:52 +0300

    switch to latest, ustable beta

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   semi: true,
   tabWidth: 2,
-  singleQuote: true,
   plugins: [
-    "prettier-plugin-svelte",
-    "prettier-plugin-organize-imports",
-    "prettier-plugin-tailwindcss",
+    'prettier-plugin-svelte',
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-tailwindcss',
   ],
 };
 
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
   // import Greet from "./lib/Greet.svelte";
+  let count = $state(0);
+  const increase = () => (count += 1);
+  const decrease = () => (count -= 1);
 </script>
 
 <main class="h-screen w-screen">
-  <h1 class="flex h-full w-full items-center justify-center">Scarab</h1>
+  <h1 class="text-center text-black">Scarab</h1>
+
+  <div class="py-10"></div>
+
+  <div class="flex justify-center gap-4 text-black">
+    <button onclick={increase}>+</button>
+    <p>{count}</p>
+    <button onclick={decrease}>-</button>
+  </div>
 </main>
 
 <style>
